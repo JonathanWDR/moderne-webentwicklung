82da17ad1f1e9e6ad496ac0357f2302268c47cbe
Tarnbir Singh <151563080+TarnbirSingh@users.noreply.github.com>
2023-12-07 20:16:11 +0100

    lulu 123

--- se_projekt/src/App.tsx
+++ se_projekt/src/App.tsx
@@ -1,4 +1,25 @@
 import React, { useState } from 'react';
+
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { mainListItems, secondaryListItems } from './listItems';
+
 let turn: boolean = true;
 let winnerstatus = false;
 
@@ -104,6 +125,24 @@ function App() {
     //     </tbody>
     //   </table>
     // </div>
+        <ThemeProvider theme={defaultTheme}>
+          <Box component="main" sx={{ display: 'flex' }}>
+            <Grid container spacing={3}>
+            <Paper
+                sx={{
+                  p: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 240,
+                }}
+              >
+                <IconButton aria-label="delete">
+                <DeleteIcon />
+                </IconButton>
+              </Paper>
+            </Grid>
+          </Box>
+        </ThemeProvider>
   );
 }
 export default App;
